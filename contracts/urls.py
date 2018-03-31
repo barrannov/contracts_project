4d365fc3bd1dbc42f46dcbb81b3c778f35537f9e
Aleksandr Baranov <abaranov@e1r7p10.unit.ua>
2018-03-31 21:11:11 +0300

    some new authentication functionality

--- contracts/urls.py
+++ contracts/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
+from django.contrib.auth.views import logout, login
 
 app_name = 'contracts'
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-    path('registration_form/', views.UserFormView.as_view(), name='registration_form')
-
+    path('registration_form/', views.UserFormView.as_view(), name='registration_form'),
+    path('login/', login, name='login'),
+    path('logout/', logout, {'next_page': '/'}, name='logout')
 ]
